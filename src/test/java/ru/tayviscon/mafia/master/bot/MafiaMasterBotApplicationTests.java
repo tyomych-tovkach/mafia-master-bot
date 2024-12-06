7743ac91343a87ed5f90c3595a83ee307e731080
tyomych-tovkach <tyomych.tovkach@tayviscon.com>
2024-12-06 08:17:16 +0300

    Начало работы над MafiaMasterBot

---
+++ src/test/java/ru/tayviscon/mafia/master/bot/MafiaMasterBotApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.tayviscon.mafia.master.bot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MafiaMasterBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
