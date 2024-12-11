5a9813c00ce9309fade0bc6f71594bb0761102d3
tyomych-tovkach <tyomych.tovkach@tayviscon.com>
2024-12-11 07:45:18 +0300

    Добавь отображение главного меню бота

--- src/main/java/ru/tayviscon/mafia/master/bot/service/handler/control/command/StartTelegramBotCommandHandler.java
+++ src/main/java/ru/tayviscon/mafia/master/bot/service/handler/control/command/StartTelegramBotCommandHandler.java
@@ -1,14 +1,20 @@
 package ru.tayviscon.mafia.master.bot.service.handler.control.command;
 
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import ru.tayviscon.mafia.master.bot.service.ui.MainMenuTelegramBotUseCase;
 
+@Log4j2
 @Component
+@RequiredArgsConstructor
 public class StartTelegramBotCommandHandler implements TelegramBotCommandHandler {
 
     public static final String NAME = "start";
+    private final MainMenuTelegramBotUseCase mainMenuTelegramBotUseCase;
 
     @Override
     public String getName() {
@@ -23,7 +29,7 @@ public class StartTelegramBotCommandHandler implements TelegramBotCommandHandler
     @Override
     @SneakyThrows
     public void handle(Update update, TelegramLongPollingBot telegramBot) {
-        //TODO: написать обработчик для команды '/start'
+        mainMenuTelegramBotUseCase.exec(update, telegramBot);
     }
 
 
